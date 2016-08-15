36b8140286a32e1940450ec132382d97675a2c63
Rajeev Srivastava <rs@efabless.com>
2016-08-15 12:25:27 -0700

    updated script to run placer graywolf

--- TESTS/vedic.tcl
+++ TESTS/vedic.tcl
@@ -1,11 +1,17 @@
-read_lef -lef /home/ubuntu/ef-xfab-xh035/EFXH035A/libs.ref/techLEF/v6_0_1/xh035_xx2x_METAL4.lef -tech only 
-read_lef -lef /home/ubuntu/ef-xfab-xh035/EFXH035A/libs.ref/lef/D_CELLS/xh035_D_CELLS_qrouter.lef
+read_lef -lef /ef/tech/XFAB/EFXH035A/libs.ref/techLEF/v6_0_1/xh035_xx2x_METAL4.lef -tech only 
+read_lef -lef /ef/tech/XFAB/EFXH035A/libs.ref/lef/D_CELLS/xh035_D_CELLS_qrouter.lef
 
-read_verilog -v /home/ubuntu/workarea/efsynthesis.vg
+read_verilog -v rtls/vedic/vedic.gv
 set TOP_MODULE vedic_16x16
 elaborate
 
 set_floorplan 
 
-write_graywolf_cel_file
-exit
+#write_graywolf_cel_file
+#read_graywolf_placement_result
+
+place_graywolf
+
+#write_def -output vedic.def
+
+#defIn -def vedic.def
