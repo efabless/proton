fbbebc742ba50e43cc38c7c4dcad97c7ad29ba72
Rajeev Srivastava <rs@efabless.com>
2016-10-28 12:36:59 -0700

    removed files not needed

--- TESTS/test-flplan-ubuntu.tcl
+++
@@ -1,4 +0,0 @@
-read_lef -lef /home/ubuntu/proton/TESTS/library/NangateOpenCellLibrary_PDKv1_2_v2008_10.lef  -tech also
-
-read_verilog -v /home/ubuntu/proton/TESTS/netlists/fifo.vg
-elaborate
--- TESTS/test-flplan.tcl
+++
@@ -1,4 +0,0 @@
-read_lef -lef /ef/home/rs/proton/TESTS/library/NangateOpenCellLibrary_PDKv1_2_v2008_10.lef  -tech also
-
-read_verilog -v /ef/home/rs/proton/TESTS/netlists/fifo.vg
-elaborate
